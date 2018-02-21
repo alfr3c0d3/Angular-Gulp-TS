ad3c9fc8be4d8c4369d58e2911b56d7899e40ee5
Alfredo Morales <C-AMorales@bkuna.com>
2018-02-21 17:36:09 -0500

    Nav Already Working

--- src/client/app/layout/shell.controller.ts
+++ src/client/app/layout/shell.controller.ts
@@ -1,12 +1,27 @@
 namespace app.layout {
   'use strict';
-  
+
+  export interface IUser {
+    id?: number,
+    // age?: number,
+    firstName?: string,
+    lastName?: string,
+    // city?: string,
+    // state?: string,
+    // zip?: string,
+    thumbnail?: string,
+    // userName?: string,
+    // password?: string,
+    role?: string
+  }
+
   export class ShellController {
     static $inject: Array<string> = ['$rootScope', '$timeout', 'config', 'logger', '$state', '$scope'];
 
     private layout: string;
     private navline: {};
-    private nav: string; 
+    private user = <IUser>{} ;
+    private headerTitle: string;
 
 
     constructor(private $rootScope: any,
@@ -19,33 +34,35 @@ namespace app.layout {
       this.logger.success(config.appTitle + ' loaded!', null);
       this.hideSplash();
       this.$rootScope.showSplash = true;
-      
+
       this.layout = sessionStorage.getItem("layout") ? sessionStorage.getItem("layout") : "layout1";
 
-      this.$scope.$on('refreshLayout', _ => {
-        this.applyChanges();
-      });
+      // this.$scope.$on('refreshLayout', _ => {
+      //   this.applyChanges();
+      // });
 
-      this.$scope.$on('signedIn', (event, data) => {
-        this.$timeout(() => {
-          this.nav = data;
-          this.$state.go('home2')
-          // $scope.$broadcast("refreshDirective");
-         })  
+      this.$scope.$on('signedIn', _ => {
+        this.applyChanges();
       });
 
       this.activate();
     }
 
+
+
     applyChanges() {
       this.$timeout(() => {
-        this.layout = sessionStorage.getItem("layout");
-        this.activate();
-       })
+        // this.layout = sessionStorage.getItem("layout");
+            this.activate();
+      })
     }
 
     activate() {
-      
+      this.user = JSON.parse(sessionStorage.getItem("user")) || this.user;
+      this.headerTitle = this.user.role ? `Welcome ${this.user.firstName} ${this.user.lastName} !` : 'Place an Order Now!'
+      if(this.user.role) 
+        this.$state.go("home2");
+
       // var statePromise = (this.layout === 'layout1') ?  this.$state.go('dashboard') : this.$state.go('people', { isFromState: true } );
 
       // statePromise.then(_ => {
@@ -62,6 +79,14 @@ namespace app.layout {
 
     signIn() {
       this.$state.go("signIn");
+    }
+
+    signOut() {
+      this.$timeout( () => {
+        sessionStorage.setItem("user", null)
+        this.user = <IUser>{};
+        this.activate();
+      });
     }
 
     hideSplash() {
--- src/client/app/signIn/signIn.controller.ts
+++ src/client/app/signIn/signIn.controller.ts
@@ -1,16 +1,29 @@
 namespace app.signIn {
   'use strict';
 
-  export class SignInController  {
-    static $inject: Array<string> = ['$rootScope', 'logger'];
+  export class SignInController {
+    static $inject: Array<string> = ['$rootScope', 'logger', 'dataservice'];
     private userName: string;
-    constructor(private $rootScope: ng.IRootScopeService, private logger: blocks.logger.ILogger) {}
+    private password: string;
+    constructor(private $rootScope: ng.IRootScopeService, private logger: blocks.logger.ILogger, private dataservice: app.core.IDataService) { }
 
     signIn() {
-      if(this.userName)
-        this.$rootScope.$broadcast("signedIn", this.userName);
-      else
-        this.logger.warning("Enter a username");
+
+      if (!this.userName) {
+        this.logger.error("Username is empty")
+        return;
+      }
+
+      this.dataservice.getPeople()
+        .then(response => {
+          var user = _.find(response, (item: any) => {
+            return item.userName === this.userName;
+          });
+
+          sessionStorage.setItem("user", JSON.stringify(user));
+          this.$rootScope.$broadcast("signedIn");
+        })
+        .catch(() => { this.logger.error("Sing in failed, please try again") })
     }
   }
 
